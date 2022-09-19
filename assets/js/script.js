cc2547f15cade5bc7f26251648d990a9cfa172ea
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2022-09-19 21:10:30 +0300

    Menu button added for mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 function changeTitle(val){
 	document.title = val;
 }
+function toggleMenu(menu){
+	menu.classList.toggle("opened");
+	//document.querySelector(".nav-links").classList.toggle("opened");
+	var navLinks = document.querySelector(".nav-links");
+	navLinks.style.display = navLinks.style.display == "block" ? "none" : "block";
+	
+}
